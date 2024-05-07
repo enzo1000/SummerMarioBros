30efc476ca79340675c8db0a37076b1c25d4269d
Ame (Adjamé T.R) <adjame.tel.roz@gmail.com>
2024-05-07 16:17:43 +0200

    saut corriger (puissance impact au plafond et rebond ennemi)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,91 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    public LayerMask groundLayer;
-    private Animator anim;
+    private Rigidbody2D rb; // Référence au Rigidbody2D
+    private Animator anim; // Référence à l'Animator
 
-    public float speed;
-    public float gravity;
-    public float jumpVelocity = 20;
+    public float speed; // Vitesse de déplacement   
+    public float jumpHeight = 6f; // Hauteur désirée pour le saut
+    public float timeToJumpApex = 0.6f; // Temps pour atteindre le point le plus haut du saut
 
-    private float Move;
-    private Vector2 velocity;
-    
-    private bool isFacingRight;
-    public bool isGrounded = true;
+    private float gravity; // Gravité calculée
+    private float jumpVelocity; // Vélocité de saut calculée
+
+    private float Move; // Stocke la valeur de l'axe horizontal
+    private bool isFacingRight; // Stocke la direction du joueur
+    private GroundedTest groundedTester; // Référence au script GroundedTest
 
     void Start()
     {
         isFacingRight = true;
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
-        velocity = rb.velocity;
+
+        // Calcul de la gravité et de la vélocité de saut basées sur la hauteur de saut et le temps pour atteindre l'apex
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+
+        groundedTester = GetComponentInChildren<GroundedTest>(); // Récupère le script GroundedTest attaché à l'enfant du joueur
     }
 
     void Update()
     {
-
-
         Move = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(Move * speed, rb.velocity.y);
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
-        {
-            velocity.y = jumpVelocity;
-            rb.velocity = new Vector2(rb.velocity.x, velocity.y);
-            isGrounded = false;
+        rb.velocity = new Vector2(Move * speed, rb.velocity.y); // Déplacement horizontal
 
-        }
-        //Animation
-        if (Move != 0)
+        if (Input.GetKeyDown(KeyCode.Space) && groundedTester.isGrounded)
         {
-            anim.SetBool("isRunning", true);
-        }
-        else
-        {
-            anim.SetBool("isRunning", false);
+            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity); // Saut
         }
 
-        if (!isFacingRight && Move > 0)
+        anim.SetBool("isRunning", Mathf.Abs(Move) > 0); // Animation de course
+
+        if ((!isFacingRight && Move > 0) || (isFacingRight && Move < 0))
         {
-            Flip();
-        }
-        else if (isFacingRight && Move < 0)
-        {
-            Flip();
+            Flip(); // Retourne le joueur si nécessaire
         }
     }
-
 
     void FixedUpdate()
     {
-        if (!isGrounded)
+        // Applique la gravité calculée lorsque le joueur n'est pas au sol
+        if (!groundedTester.isGrounded)
         {
-            velocity.y += gravity * Time.fixedDeltaTime;
-            rb.velocity = new Vector2(rb.velocity.x, velocity.y);
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + gravity * Time.fixedDeltaTime); // Applique la gravité
         }
     }
 
-
-    public void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-           isGrounded = true;
-        }
-
-    }
-
-    
-
-    public void Flip()
+    void Flip()
     {
         isFacingRight = !isFacingRight;
         Vector3 localScale = transform.localScale;
-        localScale.x *= -1f;
+        localScale.x *= -1;
         transform.localScale = localScale;
     }
-
 }
